04f7fe60c38fd641b397d0c03ffc65a90cc55433
Skumfidusen1992 <Skumfidusen1992@gmail.com>
2014-09-30 12:17:33 +0200

    created simple webserver - Anders

--- src/ca2_3semester/App.java
+++ src/ca2_3semester/App.java
@@ -5,17 +5,23 @@
  */
 package ca2_3semester;
 
+import java.io.IOException;
+import server.webServer;
+
 /**
  *
  * @author Christoffer
  */
 public class App {
+    static int port = 8080;
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException
+    {
+        webServer server = new webServer(8080);
+        server.start();
+        System.out.println("Server started, listening on port: " + port);
     }
-    
 }
