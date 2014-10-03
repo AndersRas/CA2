1af4c35f026c3454640301a7a27e5ee5d769cde5
Skumfidusen1992 <Skumfidusen1992@gmail.com>
2014-10-03 09:42:57 +0200

    deleted unnecesary code from webserver class - Anders

--- src/server/webServer.java
+++ src/server/webServer.java
@@ -5,20 +5,18 @@
  */
 package server;
 
-import Entity.Person;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import java.io.BufferedReader;
+import exceptions.NotFoundException;
+import facade.Facade;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.List;
 import java.util.Scanner;
-import javax.persistence.EntityManager;
-import javax.persistence.Persistence;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -28,11 +26,12 @@ public class webServer {
 
     private final HttpServer server;
     private final Gson gson = new Gson();
+    private Facade facade;
 
     public webServer(int port) throws IOException {
         server = HttpServer.create();
         server.bind(new InetSocketAddress(port), 0);
-        server.createContext("/connect", createContext());
+        server.createContext("/person", createContext());
     }
 
     public void start() {
@@ -54,17 +53,27 @@ public class webServer {
                 
                 if("GET".equalsIgnoreCase(he.getRequestMethod())){
                     if (lastIndexOf > 0){
-                        int id = Integer.parseInt(path.substring(lastIndexOf + 1));
-                        getPersonAsJSON(id, he);
-                        return;
+                        try {
+                            he.sendResponseHeaders(200, 0);
+                            int id = Integer.parseInt(path.substring(lastIndexOf + 1));
+                            OutputStream responseBody = he.getResponseBody();
+                            responseBody.write(facade.getPersonAsJSON(id).getBytes());
+                            return;
+                        } catch (NotFoundException ex) {
+                            Logger.getLogger(webServer.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
-                    getPersonsAsJSON(he);
+                    facade.getPersonsAsJSON();
                 }
                 
                 if("DELETE".equalsIgnoreCase(he.getRequestMethod())){
                     if (lastIndexOf > 0){
-                        int id = Integer.parseInt(path.substring(lastIndexOf + 1));
-                        deletePerson(id);
+                        try {
+                            int id = Integer.parseInt(path.substring(lastIndexOf + 1));
+                            facade.delete(id);
+                        } catch (NotFoundException ex) {
+                            Logger.getLogger(webServer.class.getName()).log(Level.SEVERE, null, ex);
+                        }
                     }
                 }
                 
@@ -76,106 +85,12 @@ public class webServer {
                         message += scanner.nextLine();
                     }
                     if(path.substring(lastIndexOf + 4).equalsIgnoreCase("Role")){
-                        addRoleFromGson(path, lastIndexOf);
+                        facade.addRoleFromGson(path, lastIndexOf);
                         return;
                     }
-                    addPersonFromJson(new Gson().fromJson(message, Person.class), he);
+                    facade.addPersonFromGson(message);
                 }
             }
         };
-    }
-
-    public void getPersonsAsJSON(HttpExchange he) throws IOException 
-    {
-        EntityManager em = Persistence.createEntityManagerFactory("Ca2_3semesterPU").createEntityManager();
-        String response = "";
-        int status = 200;
-        String method = he.getRequestMethod().toUpperCase();
-        switch (method) 
-        {
-            case "GET":
-                try 
-                {
-                    String path = he.getRequestURI().getPath();
-                    int lastIndex = path.lastIndexOf("/");
-                    if (lastIndex > 0) 
-                    {
-                        String idStr = path.substring(lastIndex + 1);
-                        int id = Integer.parseInt(idStr);
-                        List<Person> persons = em.createQuery("select p from Person p").getResultList();
-                    }
-                } catch (NumberFormatException nfe) 
-                {
-                    response = "Id is not a number";
-                    status = 404;
-                }
-                break;
-        }
-    }
-
-    public void getPersonAsJSON(long id, HttpExchange he) throws IOException 
-    {
-        EntityManager em = Persistence.createEntityManagerFactory("Ca2_3semesterPU").createEntityManager();
-        String response = "";
-        int status = 200;
-        String method = he.getRequestMethod().toUpperCase();
-        switch (method) {
-            case "GET":
-                try 
-                {
-                    String path = he.getRequestURI().getPath();
-                    int lastIndex = path.lastIndexOf("/");
-                    if (lastIndex > 0) 
-                    {
-                        String idStr = path.substring(lastIndex + 1);
-                        id = Integer.parseInt(idStr);
-                        List<Person> persons = em.createQuery("select p from Person p where id == " + id).getResultList();
-                    }
-                } catch (NumberFormatException nfe) 
-                {
-                    response = "Id is not a number";
-                    status = 404;
-                }
-                break;
-        }
-    }
-    
-    public void addPersonFromJson(String json, HttpExchange he) throws IOException 
-    {
-        EntityManager em = Persistence.createEntityManagerFactory("Ca2_3semesterPU").createEntityManager();
-        String response = "";
-        int status = 200;
-        String method = he.getRequestMethod().toUpperCase();
-        switch (method) {
-          case "POST":
-          try{
-          InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
-          BufferedReader br = new BufferedReader(isr);
-          String jsonQuery = br.readLine();
-          if(jsonQuery.contains("<") || jsonQuery.contains(">")){
-            throw new IllegalArgumentException("Illegal characters in input");
-          }
-          
-//          response = new Gson().toJson(p);
-          }catch(IllegalArgumentException iae){
-            status = 400;
-            response = iae.getMessage();
-          }
-          catch(IOException e){
-            status = 500;
-            response = "Internal Server Problem";
-          }
-          break;
-    }
-    }
-    
-    
-    public void addRoleFromGson(String json, long id)
-    {
-        
-    }
-    
-    public void deletePerson(int id){
-        
     }
 }
